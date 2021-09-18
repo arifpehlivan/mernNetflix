d6db034359a6e11e16ace3a711093090d93506a9
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-09-18 23:41:09 +0300

    netflix

--- api/index.js
+++ api/index.js
@@ -2,15 +2,21 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
 mongoose.connect(process.env.mongo_url, {
     useNewUrlParser:true,
     useUnifiedTopology:true,
+    //useCreateIndex:true,
 })
 .then(()=> console.log("DB Connection Succesfull!"))
 .catch((err)=>console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, ()=>{
     console.log("Backend server is running!");
